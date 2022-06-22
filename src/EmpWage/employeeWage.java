a0e02b9adae263932d3861082f71a33bf179694e
Hiteshmeher <meher.hitesh4@gmail.com>
2022-06-22 22:25:35 +0530

    Solving using Switch Case

--- src/EmpWage/employeeWage.java
+++ src/EmpWage/employeeWage.java
@@ -2,25 +2,41 @@ package EmpWage;
 
 public class employeeWage {
 
-	static int WagePerHour = 20;
-    static int FullDayHour = 8;
-    static int PartTimeHour = 4;
+	static final int WagePerHour = 20;
+	static final int FullDayHour = 8;
+	static final int PartTimeHour = 4;
+	static final int IsPartTime = 0;
+	static final int IsFulltime = 1;
+	static final int WorkingDayInMonth = 20;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Problem::");
-        int dailyWages = 0;
-        int attendance = (int) Math.floor(Math.random()*10)%3;
-        if (attendance == 0)
-            System.out.println("Emloyee is absent");
-        else if (attendance == 1) {
-            System.out.println("Employee is present");
-            dailyWages = WagePerHour * FullDayHour;
-            System.out.println("Wages of the day is  "+dailyWages);
-        }
-        else {
-            System.out.println("Employee is present for the Half day");
-            dailyWages = WagePerHour * PartTimeHour;
-            System.out.println("Wages of the day for part time "+dailyWages);
-        }
-    }
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Problem::");
+		int dailyWages = 0;
+		int Monthlywages = 0;
+		int workingDay = 1;
+		int day = 1;
+		while (workingDay <= WorkingDayInMonth) {
+			int attendance = (int) Math.floor(Math.random() * 10) % 3;
+			switch (attendance) {
+			case IsPartTime:
+				System.out.println("Day " + day + " Half day");
+				dailyWages = WagePerHour * PartTimeHour;
+				System.out.println("Wages of the day " + day + " is " + dailyWages);
+				Monthlywages = dailyWages + Monthlywages;
+				break;
+			case IsFulltime:
+				System.out.println("Day " + day + " present");
+				dailyWages = WagePerHour * FullDayHour;
+				System.out.println("Wages of the day " + day + " is " + dailyWages);
+				Monthlywages = dailyWages + Monthlywages;
+				break;
+			default:
+				System.out.println("Day " + day + " Absent");
+				break;
+			}
+			day++;
+			workingDay++;
+		}
+		System.out.println("Wages fot the month is " + Monthlywages);
+	}
 }
